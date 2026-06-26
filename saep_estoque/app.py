0240337d9ead64f8bfa67dcaac5cf9106bb06782
Danillo686 <uqualquer220@gmail.com>
2026-06-26 16:53:55 -0300

    Add files via upload

--- saep_estoque/app.py
+++ saep_estoque/app.py
@@ -1,18 +1,39 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox 
 from banco import cadastrar_produto
+from banco import listar_produtos
+from banco import excluir_produto
 
 janela = tk.Tk()
 janela.title("Sistema de Estoque")
-janela.geometry("800x600")
+janela.geometry("900x900")
 
+tabela = ttk.Treeview(janela, columns=('id', 'nome', 'categoria', 'quantidade', 'preco'), show='headings')
+
+tabela.heading('id', text='ID')
+tabela.heading('nome', text='Nome')
+tabela.heading('categoria', text='Categoria')
+tabela.heading('quantidade', text='Quantidade')
+tabela.heading('preco', text='Preço')
+tabela.pack(pady=10)
+
+tabela.column('id', width=50)
+tabela.column('nome', width=200)
+tabela.column('categoria', width=150)
+tabela.column('quantidade', width=100)
+tabela.column('preco', width=100)
+tabela.pack(pady=10)
+
+#Titulo
 titulo = tk.Label(
     janela, 
     text = " Sistema de Controle de Estoque ", 
     font=("Arial", 18, "bold")
-)
-titulo.pack(pady = 10)
+)   
+ 
 
+titulo.pack(pady = 10)
 frame_nome = tk.Frame(janela)
 frame_nome.pack(pady=5)
 
@@ -36,6 +57,21 @@ tk.Label(janela, text="Preço: ").pack()
 entrada_preco = tk.Entry(janela, width=40)
 entrada_preco.pack()
 
+#Atualizar tabela
+def atualizar_tabela():
+    for item in tabela.get_children():
+        tabela.delete(item)
+    
+    produtos = listar_produtos()
+    for produto in produtos:
+        tabela.insert('', tk.END, values=produto)
+        messagebox.showinfo("Sucesso", "Produto atualizado com sucesso") 
+    entrada_nome.delete(0, tk.END)
+    entrada_categoria.delete(0, tk.END)
+    entrada_quantidade.delete(0, tk.END)
+    entrada_preco.delete(0, tk.END)
+atualizar_tabela()
+
 def salvar():
     nome = entrada_nome.get()
     categoria = entrada_categoria.get()
@@ -45,7 +81,6 @@ def salvar():
     if nome == "" or categoria == "" or quantidade == "" or preco == "":
         messagebox.showwarning("Atenção", "Todos os campos devem ser preenchidos")
         return 
-
     cadastrar_produto(nome, categoria, int(quantidade), float(preco))
 
     messagebox.showinfo("Sucesso", "Produto cadastrado com sucesso!")
@@ -53,9 +88,28 @@ def salvar():
     entrada_categoria.delete(0, tk.END)
     entrada_quantidade.delete(0, tk.END)
     entrada_preco.delete(0, tk.END)
+    atualizar_tabela()
 
-
-botao_salvar = tk.Button(janela, text="Cadastrar Produto", command=salvar, width    = 25)
+botao_salvar = tk.Button(janela, text="Cadastrar Produto", command=salvar, width = 25)
 botao_salvar.pack(pady=10)
+
+#Excluir
+def excluir():
+    item_selecionado = tabela.selection()
+    if not item_selecionado:
+        messagebox.showwarning("Atenção", "Selecione um produto para excluir")
+        return
+    
+    item = tabela.item(item_selecionado)
+    id_produto = item['values'][0]
+    excluir_produto(id_produto)
+    atualizar_tabela()
+
+    messagebox.showinfo("Sucesso", "Produto excluído com sucesso")
+
+#Botão Excluir
+botao_excluir = tk.Button(janela, text="Excluir Produto", command=excluir, width = 25)
+botao_excluir.pack(pady=10)
+
 
 janela.mainloop()
--- saep_estoque/banco.py
+++ saep_estoque/banco.py
@@ -25,3 +25,46 @@ def cadastrar_produto(nome, categoria, quantidade, preco):
 
     cursor.close()
     conexao.close()
+
+
+#Busca de dados! ->
+
+def listar_produtos():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("SELECT id, nome, categoria, quantidade, preco FROM produtos")
+    produtos = cursor.fetchall()
+    
+    cursor.close()
+    conexao.close()
+    return produtos
+
+
+#Atualizar dados! ->
+
+def atualizar_produto(id_produto, nome, categoria, quantidade, preco):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = "UPDATE produtos SET nome = %s, categoria = %s, quantidade = %s, preco = %s WHERE id = %s;"
+    valores = (nome, categoria, quantidade, preco, id_produto)
+
+    cursor.execute(sql, valores)
+    conexao.commit()
+
+    cursor.close()
+    
+
+# EXcluir dados! ->    
+def excluir_produto(id_produto):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = "DELETE FROM produtos WHERE id = %s;"
+    cursor.execute(sql, (id_produto,))
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+    conexao.close()
